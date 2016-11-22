de51690aac7510d9893354d87b07ee830e5df8b9
ZeroTwo <edwards.rob@gmail.com>
2016-11-22 21:21:30 +0000

    Fix BTBLib
    Updated BTBLib nodes.  Enum to flag.

--- BTBLib/Battle/Node.cs
+++ BTBLib/Battle/Node.cs
@@ -11,10 +11,16 @@ namespace BTBLib
         public uint Radius { get; set; }
         public uint Direction { get; set; }
         public uint NodeID { get; set; }
-        public uint UUID { get; set; }
+        public uint UnitID { get; set; }
         public uint ScriptFunc { get; set; }
 
-        public enum USAGE { UNKNOWN4, DEPLOYMENT, UNKNOWN2, NONE }
+        [Flags] public enum  USAGE { CENTREDPOINT=1,
+                                     ISUNIT = 2,
+                                     WAYPOINT = 4,
+                                     UNK8 =8, UNK16=16, UNK32=32, UNK64=64,
+                                     UNK128 =128, UNK256=256, UNK512=512,
+                                     UNK1024 =1024, UNK2048=2048, UNK4096=4096,
+                                     UNK8192=8192, UNK16384=16384, UNK32768=32768 }
         public USAGE Usage { get; set; }
 
         public Node(int x, 
@@ -31,7 +37,7 @@ namespace BTBLib
             this.Radius = radius;
             this.Direction = direction;
             this.NodeID = nodeID;
-            this.UUID = UUID;
+            this.UnitID = UUID;
             this.ScriptFunc = scriptFunc;
             this.Usage = usage;
         }
